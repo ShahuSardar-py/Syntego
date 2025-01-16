b8e48da51500a6c8b766302d1bff8ab398a6fce6
ShahuSardar-py <shahu.sardar4@gmail.com>
2025-01-16 18:49:50 +0530

    Added backend logic functions

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+from pages import addEx,view,summary
+st.set_page_config(page_title="Syntego",
+                   page_icon="💲")
+
+
+st.title(" hello world-syntego 👋")
